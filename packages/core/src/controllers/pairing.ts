e1311bf71ba1819050c35c7d206a8046e2a1ad44
Ezenwankwo Gabriel <ezenwankwogabriel@gmail.com>
2024-01-25 13:11:42 +0100

    fix: do not override topic

--- packages/core/src/controllers/pairing.ts
+++ packages/core/src/controllers/pairing.ts
@@ -134,7 +134,7 @@ export class Pairing implements IPairing {
 
     // avoid overwriting keychain pairing already exists
     if (!this.core.crypto.keychain.has(topic)) {
-      await this.core.crypto.setSymKey(symKey, topic);
+      await this.core.crypto.setSymKey(symKey);
       await this.core.relayer.subscribe(topic, { relay });
     }
 
